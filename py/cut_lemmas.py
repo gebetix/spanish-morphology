9ec075f8293bff620c312763b176104b0ed147c3
yurap <angry-yura@ya.ru>
2012-10-21 00:48:20 +0400

    added XCut rule

--- py/cut_lemmas.py
+++ py/cut_lemmas.py
@@ -12,24 +12,44 @@ def from_stdin():
 def runLemmasHeuristics(lemmas):
 	newLemmas = []
 	hasVerbOfFirst = False
+	shortestNoun = ''
 	for lemma in lemmas:
 		if lemma[-1] == 'V' and lemma[-4] == 'a':
 			hasVerbOfFirst = True
 			return [lemma]
 
+		elif lemma[-1] == 'N':
+			if shortestNoun == '' or len(lemma) < len(shortestNoun):
+				shortestNoun = lemma
+
 	for lemma in lemmas:
-		if lemma[-1] == 'A':
+		if lemma[-1] == 'A' or lemma[-1] == 'N':
 			continue
 
 		newLemmas.append(lemma)		
 
+	if len(shortestNoun) > 0:
+		newLemmas.append(shortestNoun)
+
+	return newLemmasdef runXCut(lemmas):newLemmas = []
+	hasX = False
+	for lemma in lemmas:
+		parts = lemma.split('+')
+		word = parts[0]
+		if len(parts) > 2:
+	newLemmas.append(word + '+' + parts[2])
 	return newLemmas
+
 
 def printWordLemmas(word, lemmas):
 	print str(word) + '\t' + '\t'.join(lemmas)
 
 def analizeWordLemmas(word, lemmas):
-	newLemmas = runLemmasHeuristics(lemmas)
+	newLemmas = runXCut(lemmas)
+	if len(newLemmas) == 0:
+		newLemmas = runLemmasHeuristics(lemmas)
+	else:
+		newLemmas = runLemmasHeuristics(newLemmas)
 	printWordLemmas(word, newLemmas)
 
 firstWordMode = True
--- run.sh
+++ run.sh
@@ -10,14 +10,9 @@ learn() {
 	echo "\n====== Running ======\n"
 	cat data/spanish.txt.learn | awk '{print $1}' | ./foma/flookup spanish.bin > temporary.prediction.raw.result
 	echo 'Created file temporary.prediction.raw.result'
-	cat temporary.prediction.raw.result | grep -o "^[^\+]*\+.." | uniq > temporary.prediction.result
+	# cat temporary.prediction.raw.result | grep -o "^[^+]*\(+X\|\)+[VNA]" | uniq > temporary.prediction.result
+	cat temporary.prediction.raw.result | grep -o "\(^[^+]*\(+X\|\)+[VNA]\)\|\(^[^?]*?$\)" | uniq > temporary.prediction.result
 	echo 'Created file temporary.prediction.result'
-	
-	cat temporary.prediction.result | awk 'BEGIN{m=1;str=""}{if(m==1){w=$1;m=0}if($1==w){str=str"\t"$2}else{print w""str;w=$1;str="\t"$2}}END{if(length(str)>0){print w""str}}'> temporary.final.result
-	echo 'Created file temporary.final.result'
-
-	echo "\n====== Results without cutting off ======\n"
-	python py/evaluate.py data/spanish.txt.learn temporary.final.result
 }
 
 control() {
@@ -31,13 +26,9 @@ control() {
 	cat data/spanish.txt.test.clean | ./foma/flookup spanish.bin > temporary.prediction.raw.result
 	echo 'Created file temporary.prediction.raw.result'
 	
-	cat temporary.prediction.raw.result | grep -o "^[^\+]*\+.." | uniq > temporary.prediction.result
+	cat temporary.prediction.raw.result | grep -o "\(^[^+]*\(+X\|\)+[VNA]\)\|\(^[^?]*?$\)" | uniq > temporary.prediction.result
 	echo 'Created file temporary.prediction.result'
-	
-	#no need in this anymore
-	#cat temporary.prediction.result | awk 'BEGIN{m=1;str=""}{if(m==1){w=$1;m=0}if($1==w){str=str"\t"$2}else{print w""str;w=$1;str="\t"$2}}END{if(length(str)>0){print w""str}}'> temporary.final.result
-	#echo 'Created file temporary.final.result'
-	
+		
 	echo "\n====== Removing lemmas ======\n"
 	cat temporary.prediction.result | python py/cut_lemmas.py > temporary.final.cutted.result
 	echo 'Created file temporary.final.cutted.result'
@@ -52,10 +43,10 @@ cut_lemmas() {
 	echo 'Created file temporary.final.cutted.result'
 
 	echo "\n====== Results ======\n"
-	python py/evaluate.py data/spanish.txt.learn temporary.final.cutted.result	
+	python py/evaluate.py data/spanish.txt.learn temporary.final.cutted.result
 }
 
-control
+# control
 
-# learn
-# cut_lemmas
+learn
+cut_lemmas
